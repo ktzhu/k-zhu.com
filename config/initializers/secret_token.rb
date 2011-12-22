49bb53287809e4e9c044d5e843848e693e97ee41
Katie Zhu <kzhu91@gmail.com>
2011-12-22 16:23:26 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KZhuCom::Application.config.secret_token = 'a4010c6c8d8e28c043df7c52dbf8fe1c92df24ac6b4968f4ac4e9d19b0165f2bbac19b4134f2e5e3df02f0b5e615049dd6b11273ab20958f0cd60ccd88df4a6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KZhuCom::Application.config.session_store :cookie_store, key: '_k-zhu.com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KZhuCom::Application.config.session_store :active_record_store
