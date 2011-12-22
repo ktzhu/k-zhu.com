b968eab0607829efffd526cc0b244a0477d99e7d
Katie Zhu <kzhu91@gmail.com>
2011-12-22 17:15:41 -0600

    Added Project model

---
+++ app/models/project.rb
@@ -0,0 +1,13 @@
+class Project
+	include MongoMapper::Document
+
+	key :project_id, ObjectId
+	key :name, String, :required => true
+	key :blurb, String
+	key :content, String, :required => true
+	key :client, String
+	key :skills, Array
+	key :link, String
+	key :image, String
+	timestamps!
+end
