81465a1ff524d981d1b2becc552f42fae8f63735
Katie Zhu <kzhu91@gmail.com>
2011-12-23 03:17:37 -0600

    Added slug field to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,8 @@ class Project
 	include MongoMapper::Document
 
 	key :project_id, ObjectId
-	key :name, String, :required => true, :unique => true
+	key :name, String, :required => true
+	key :slug, String, :required => true, :unique => true
 	key :blurb, String
 	key :content, String, :required => true
 	key :client, String
